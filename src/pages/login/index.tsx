42ad76907b71d29db4012824ffb65a7635bf9309
matthew.lau <matthew.lau@credera.co.uk>
2023-09-21 22:34:07 +0100

    feat: add login page scaffold

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,7 +6,81 @@ interface Values {
 }
 
 const LoginPage = (): ReactElement => {
-  return <div>Login</div>
+  return (
+    <div className="flex flex-col md:flex-row h-screen items-center">
+      <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+        <img
+          src="https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          alt="house-login"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div
+        className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+        flex items-center justify-center"
+      >
+        <div className="w-full h-100">
+          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
+            Log in to your account
+          </h1>
+
+          <form className="mt-6" action="#" method="POST">
+            <div>
+              <label className="block text-gray-700">Email Address</label>
+              <input
+                type="email"
+                name=""
+                id=""
+                placeholder="Enter Email Address"
+                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                required
+              />
+            </div>
+
+            <div className="mt-4">
+              <label className="block text-gray-700">Password</label>
+              <input
+                type="password"
+                name=""
+                id=""
+                placeholder="Enter Password"
+                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                focus:bg-white focus:outline-none"
+                required
+              />
+            </div>
+
+            <div className="text-right mt-2">
+              <a
+                href="#"
+                className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700"
+              >
+                Forgot Password?
+              </a>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
+              px-4 py-3 mt-6"
+            >
+              Log In
+            </button>
+          </form>
+          <p className="mt-8">
+            Need an account?{' '}
+            <a
+              href="#"
+              className="text-blue-500 hover:text-blue-700 font-semibold"
+            >
+              Create an account
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
