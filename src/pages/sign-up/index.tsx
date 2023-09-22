e3f1abc61ba40771332305949b5ee56e8472b606
matthew.lau <matthew.lau@credera.co.uk>
2023-09-22 11:43:16 +0100

    feat: scaffolding for sign up page

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,12 +1,87 @@
 import { ReactElement } from 'react'
-
+import { useNavigate } from 'react-router-dom'
 interface Values {
   email: string
   password: string
 }
 
 const SignUpPage = (): ReactElement => {
-  return <div>Sign-up</div>
+  const navigate = useNavigate()
+  return (
+    <div className="flex flex-col md:flex-row h-screen items-center">
+      <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+        <img
+          src="https://images.unsplash.com/photo-1564013799919-ab600027ffc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
+          alt="house-sign-up"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div
+        className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+        flex items-center justify-center"
+      >
+        <div className="w-full h-100">
+          <h1 className="text-xl md:text-2xl leading-tight mt-12">
+            Sign up now!
+          </h1>
+
+          <form className="mt-6" action="#" method="POST">
+            <div>
+              <label className="block text-gray-700">Email Address</label>
+              <input
+                type="email"
+                name=""
+                id=""
+                placeholder="Enter email address"
+                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                required
+              />
+            </div>
+
+            <div className="mt-4">
+              <label className="block text-gray-700">Password</label>
+              <input
+                type="password"
+                name=""
+                id=""
+                placeholder="Enter password"
+                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                focus:bg-white focus:outline-none"
+                required
+              />
+            </div>
+
+            <div className="mt-4">
+              <label className="block text-gray-700">Confirm Password</label>
+              <input
+                type="password"
+                name=""
+                id=""
+                placeholder="Enter password again"
+                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                focus:bg-white focus:outline-none"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white rounded-lg
+              px-4 py-3 mt-6"
+            >
+              Sign up
+            </button>
+          </form>
+          <button
+            onClick={() => navigate('/')}
+            className="mt-4 text-blue-500 hover:text-blue-700"
+          >
+            Return home
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SignUpPage
