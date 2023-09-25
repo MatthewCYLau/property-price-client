90f32c4a35ea0d99081b9bc39eb4571f45d168bf
matthew.lau <matthew.lau@credera.co.uk>
2023-09-25 14:14:30 +0100

    fix: add redirect

--- src/components/side-nav/index.tsx
+++ src/components/side-nav/index.tsx
@@ -5,7 +5,7 @@ const Sidebar: FC = () => {
     <aside className="hidden inset-y-0 z-10 flex flex-col flex-shrink-0 w-64 max-h-screen overflow-hidden transition-all transform bg-white border-r shadow-lg z-auto md:block md:static shadow">
       <div className="flex items-center justify-between flex-shrink-0 p-2">
         <span className="p-2 text-xl font-semibold leading-8 tracking-wider uppercase whitespace-nowrap">
-          Property Engine
+          P' Price Engine
         </span>
       </div>
       <nav className="flex-1 overflow-hidden hover:overflow-y-auto">
