5915912f03d408262b43057415776568209f0011
matthew.lau <matthew.lau@credera.co.uk>
2023-09-26 21:44:24 +0100

    feat: add nav button

--- src/components/side-nav/index.tsx
+++ src/components/side-nav/index.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 import HomeIcon from '../home-icon'
+import DocumentIcon from '../document-icon'
+
 const Sidebar: FC = () => {
   return (
     <aside className="hidden inset-y-0 z-10 flex flex-col flex-shrink-0 w-64 max-h-screen overflow-hidden transition-all transform bg-white border-r shadow-lg z-auto md:block md:static shadow">
@@ -11,15 +14,28 @@ const Sidebar: FC = () => {
       <nav className="flex-1 overflow-hidden hover:overflow-y-auto">
         <ul className="p-2 overflow-hidden">
           <li>
-            <a
-              href="#"
+            <Link
+              to="/dashboard"
               className="flex items-center p-2 space-x-2 rounded-md hover:bg-gray-100"
             >
               <span>
                 <HomeIcon />
               </span>
               <span>Dashboard</span>
-            </a>
+            </Link>
+          </li>
+        </ul>
+        <ul className="p-2 overflow-hidden">
+          <li>
+            <Link
+              to="/dashboard"
+              className="flex items-center p-2 space-x-2 rounded-md hover:bg-gray-100"
+            >
+              <span>
+                <DocumentIcon />
+              </span>
+              <span>Add Property</span>
+            </Link>
           </li>
         </ul>
       </nav>
