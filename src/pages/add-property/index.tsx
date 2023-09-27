cb9065cdb40f1ddfb655b9622a8c8f096361578f
matthew.lau <matthew.lau@credera.co.uk>
2023-09-27 19:24:18 +0100

    feat: add form scaffolding

--- src/pages/add-property/index.tsx
+++ src/pages/add-property/index.tsx
@@ -44,8 +44,57 @@ const AddPropertyPage = (): ReactElement => {
 
   return (
     <Layout>
-      <div className="">
-        <form onSubmit={submitHandler}></form>
+      <div className="grid grid-cols-1 gap-5 mt-6 sm:grid-cols-2 lg:grid-cols-4">
+        <form onSubmit={submitHandler}>
+          <div className="mb-6">
+            <input
+              type="text"
+              placeholder="Email"
+              className="
+                        w-full
+                        rounded-md
+                        border
+                        bordder-[#E9EDF4]
+                        py-3
+                        px-5
+                        bg-[#FCFDFE]
+                        text-base text-body-color
+                        placeholder-[#ACB6BE]
+                        outline-none
+                        focus-visible:shadow-none
+                        focus:border-primary
+                        "
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="password"
+              placeholder="Password"
+              className="
+                        w-full
+                        rounded-md
+                        border
+                        bordder-[#E9EDF4]
+                        py-3
+                        px-5
+                        bg-[#FCFDFE]
+                        text-base text-body-color
+                        placeholder-[#ACB6BE]
+                        outline-none
+                        focus-visible:shadow-none
+                        focus:border-primary
+                        "
+            />
+          </div>
+          <div className="mb-6">
+            <button
+              type="submit"
+              className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+            >
+              Add Property
+            </button>
+          </div>
+        </form>
       </div>
     </Layout>
   )
