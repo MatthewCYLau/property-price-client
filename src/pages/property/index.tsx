85160a0e2060cc5082259297a02865bf4885af85
matthew.lau <matthew.lau@credera.co.uk>
2023-09-30 08:42:05 +0100

    feat: add price suggestion input

--- src/pages/property/index.tsx
+++ src/pages/property/index.tsx
@@ -52,6 +52,26 @@ const PropertyPage = (): ReactElement => {
           </>
         )}
       </div>
+      <div className="grid grid-cols-1 gap-5 mt-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="h-10">
+          <label className="w-full text-gray-700 text-sm font-semibold">
+            Counter Input
+          </label>
+          <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+            <button className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-1/4 rounded-l cursor-pointer outline-none">
+              <span className="m-auto text-2xl font-thin">−</span>
+            </button>
+            <input
+              type="number"
+              className="outline-none focus:outline-none text-center w-1/2 bg-gray-300 font-semibold text-md hover:text-black focus:text-black md:text-basecursor-default flex items-center text-gray-700  outline-none"
+              value="0"
+            ></input>
+            <button className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-1/4 rounded-r cursor-pointer">
+              <span className="m-auto text-2xl font-thin">+</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
