b0486a3aac1250d239188065f97d7f7fcf0c2b06
matthew.lau <matthew.lau@credera.co.uk>
2023-10-04 08:25:54 +0100

    fix: side nav styling

--- src/components/side-nav/index.tsx
+++ src/components/side-nav/index.tsx
@@ -24,7 +24,7 @@ const Sidebar: FC = () => {
   const logout = () => dispatch({ type: AuthActionType.LOGOUT })
 
   return (
-    <aside className="hidden inset-y-0 z-10 flex flex-col flex-shrink-0 w-64 max-h-screen overflow-hidden transition-all transform bg-white border-r shadow-lg z-auto md:block md:static shadow">
+    <aside className="hidden inset-y-0 z-10 flex flex-col flex-shrink-0 w-64 max-h-screen overflow-hidden transition-all transform shadow-lg z-auto md:block md:static shadow">
       <div className="flex items-center justify-between flex-shrink-0 p-2">
         <span className="p-2 text-xl font-semibold leading-8 tracking-wider uppercase whitespace-nowrap">
           P' Price Engine
