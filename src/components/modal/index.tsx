abe8b12de594c52eb6e49876c4c22744215312a5
matthew.lau <matthew.lau@credera.co.uk>
2023-10-04 08:19:37 +0100

    fix: styling for modal color

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -7,14 +7,14 @@ const Modal = () => {
     <>
       {state.modal.showModal && (
         <div className="bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-          <div className="shadow-lg bg-gray-800 rounded px-20 py-16 rounded-md text-center z-10">
-            <h1 className="text-l mb-4 font-bold text-white ">
+          <div className="shadow-lg bg-white rounded px-20 py-16 rounded-md text-center z-10">
+            <h1 className="text-l mb-4 font-bold text-grey-900">
               {state.modal.message}
             </h1>
             <div className="flex flex-col mt-4 sm:flex-row sm:items-center sm:justify-center">
               <button
                 onClick={state.modal.onCancel}
-                className="flex items-center justify-center order-1 w-full px-2 py-2 mt-3 text-sm tracking-wide text-gray-600 capitalize transition-colors duration-300 transform border rounded-md sm:mx-2 dark:border-gray-400 dark:text-gray-300 sm:mt-0 sm:w-40 hover:bg-gray-50 focus:outline-none focus:ring dark:hover:bg-gray-800 focus:ring-gray-300 focus:ring-opacity-40"
+                className="flex items-center justify-center order-1 w-full px-2 py-2 mt-3 text-sm tracking-wide text-blue capitalize transition-colors duration-300 transform border rounded-md sm:mx-2 sm:mt-0 sm:w-40 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
               >
                 Cancel
               </button>
