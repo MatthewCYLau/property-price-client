53651f6a1ff2e5299976b54d0f6d8910316827f1
matthew.lau <matthew.lau@credera.co.uk>
2023-10-08 15:38:55 +0100

    feat: update form state for update user

--- src/pages/update-user/index.tsx
+++ src/pages/update-user/index.tsx
@@ -1,7 +1,15 @@
-import { ReactElement, useState } from 'react'
+import { ReactElement, useState, useContext } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Store } from '../../store'
 import Layout from '../../components/layout'
 import { UserType } from '../../types'
 import CtaButton from '../../components/cta-button'
+
+interface Values {
+  email: string
+  password: string
+  userType: UserType
+}
 
 type UserTypeMap = {
   [name in UserType]: string
@@ -23,10 +31,20 @@ const userTypes: readonly UserType[] = [
 
 const UpdateUserPage = (): ReactElement => {
   const [showDropdown, setShowDropdown] = useState<boolean>(false)
-  const [userType, setUserType] = useState<UserType>(UserType.RENTER)
+  const { dispatch, state } = useContext(Store)
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [formValues, setFormValues] = useState<Values>({
+    email: '',
+    password: '',
+    userType: UserType.RENTER
+  })
 
   const dropdownItemOnClickHandler = (n: UserType) => {
-    setUserType(n)
+    setFormValues((prevState) => ({
+      ...prevState,
+      userType: n
+    }))
     setShowDropdown(!showDropdown)
   }
 
@@ -36,7 +54,7 @@ const UpdateUserPage = (): ReactElement => {
         <form
           onSubmit={(e) => {
             e.preventDefault()
-            console.log(userType)
+            console.log(formValues.userType)
           }}
         >
           <div className="mb-6">
@@ -45,9 +63,12 @@ const UpdateUserPage = (): ReactElement => {
           <div className="relative group">
             <button
               onClick={() => setShowDropdown(!showDropdown)}
+              type="button"
               className="inline-flex justify-center w-full px-2 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
             >
-              <span className="mr-2">{userTypeButtonCopies[userType]}</span>
+              <span className="mr-2">
+                {userTypeButtonCopies[formValues.userType]}
+              </span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-5 h-5 ml-2 -mr-1"
