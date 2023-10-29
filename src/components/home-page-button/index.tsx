9b166d3d34a58a5eb5d1454bbdb043c4be7e27f5
matthew.lau <matthew.lau@credera.co.uk>
2023-10-29 12:44:33 +0000

    fix: button styling

--- src/components/home-page-button/index.tsx
+++ src/components/home-page-button/index.tsx
@@ -16,9 +16,9 @@ const HomePageButton: FC<Props> = ({
     <button
       onClick={onClickHandler}
       className={cn(
-        'inline-flex items-center justify-center px-10 py-3 text-base font-medium text-center mr-3 last:mr-0',
+        'w-full inline-flex items-center justify-center px-10 py-3 text-base font-medium text-center mr-3 last:mr-0 sm:w-1/3',
         {
-          'text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900':
+          'mt-4 text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 sm:mt-0':
             isPrimaryCta,
           'text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800':
             !isPrimaryCta
