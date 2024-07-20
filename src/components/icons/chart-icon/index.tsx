b0b9021884db21751cc9fdd822e759082674eeb5
Matthew Lau <lau.cy.matthew@gmail.com>
2024-07-20 07:44:32 +0100

    chore: fix console error

--- src/components/icons/chart-icon/index.tsx
+++ src/components/icons/chart-icon/index.tsx
@@ -12,9 +12,9 @@ const ChartIcon: FC = () => {
     >
       <path
         d="M21 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V3M15 10V17M7 13V17M19 5V17M11 7V17"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
